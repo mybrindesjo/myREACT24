4152c46249cdd0efdf4cbfda4f5e308ec87a5041
My <my.brindesjo@gmail.com>
2025-01-28 15:20:35 +0100

    New changes 28-01-2025

---
+++ myPortfolio/src/components/ProjectCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectCard = ({ title, description, images }) => (
+    <article className="project-card">
+        <div className="project-header">
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+        {images.map((image, index) => (
+            <img key={index} className="project-img" src={image.src} alt={image.alt} />
+        ))}
+    </article>
+);
+
+export default ProjectCard;
