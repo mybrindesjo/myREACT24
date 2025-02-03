84f865a0ef2013897c0ba999b4ebcaf15fb2dd75
My <my.brindesjo@gmail.com>
2025-02-03 10:35:11 +0100

    New changes 03-02-2025

---
+++ Pokemon/src/components/Board.jsx
@@ -0,0 +1,68 @@
+import Square from "./Square";
+
+const Board = ({ xIsNext, squares, onPlay }) => {
+  const handleClick = (index) => {
+    const valueExistAtIndex = squares[index];
+    const winnerFound = calculateWinner(squares);
+
+    if (valueExistAtIndex || winnerFound) {
+      return;
+    }
+
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[index] = "X";
+    } else {
+      nextSquares[index] = "O";
+    }
+
+    onPlay(nextSquares);
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board">
+        {squares.map((value, index) => (
+          <Square
+            key={index}
+            value={value}
+            onSquareClick={() => handleClick(index)}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Board;
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+};
