7ee6eddc1bd332b6ff1d0ccc4dc9aef0193ceea1
My <my.brindesjo@gmail.com>
2025-02-07 09:24:31 +0100

    New changes 07-02-2025

--- FormReact/src/component/Form.jsx
+++ FormReact/src/component/Form.jsx
@@ -5,7 +5,11 @@ const Form = () => {
     
     return (
         <form>
-            <input placeholder="Name" onChange
+            <input placeholder="Name" onChange={handeleCange}></input>
+            <input placeholder="Email" onChange={handeleCange}></input>
+            <input placeholder="Password" onChange={handeleCange}></input>
+            <button>Submit</button>
+    
         </form>
     )
 }
