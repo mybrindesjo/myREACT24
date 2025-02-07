a5176d592df4ccef86de085256ac8c356c96ceab
My <my.brindesjo@gmail.com>
2025-02-07 09:15:53 +0100

    New changes 07-02-2025

---
+++ FormReact/src/component/Form.jsx
@@ -0,0 +1,11 @@
+const Form = () => {
+    const handeleCange = () => {
+        console.log("a chage happend");
+    };
+    
+    return (
+        <form>
+            <input placeholder="Name" onChange
+        </form>
+    )
+}
